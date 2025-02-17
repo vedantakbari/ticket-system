1cb59aa440ff87e2f6a69cfb969c8b3bc11b0f55
vedantakbari <vedantakbari@gmail.com>
2025-02-17 10:14:14 -0800

    Move global CSS impodrt to _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 // pages/index.js
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Ticket System!</h1>
+    <div className="container">
+      <div className="content">
+        <h1>Welcome to the Ticket System!</h1>
+        <p>Start a ticket below and get help instantly.</p>
+        <button>Start a Ticket</button>
+      </div>
     </div>
   );
 }
